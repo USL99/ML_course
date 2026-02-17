ff28e0e744b5d7c7b0c7db800a79dbeba01f0c93
USL99 <USL99@mail.ru>
2026-02-17 23:02:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,53 +8,52 @@ def f(x, y):
 
 def grad_descent(f, x_0, y_0, eta = 0.01, N = 1000):
     dx = dy = 0.0001
-    x, y = x_0, y_0
-    xs = [х]
+    x, y = float(x_0), float(y_0)
+    xs = [x]
     ys = [y]
     fs = [f(x, y)]
 
     for i in range(N):
-
         x_derivative = (f(x + dx, y) - f(x, y)) / dx
         y_derivative = (f(x, y + dy) - f(x, y)) / dy
 
         x_next = x - x_derivative * eta
         y_next = y - y_derivative * eta
 
-        x = x_next
-        y = y_next
-
+        x, y = x_next, y_next
 
         xs.append(x)
         ys.append(y)
         fs.append(f(x,y))
 
-        xs = np.array(xs)
-        ys = np.array(ys)
-        fs = np.array(fs)
-
-    return (xs, ys, fs)
-
-
+    return np.array(xs), np.array(ys), np.array(fs)
 
 x0 = float(input("Enter the starting point x0: "))
 y0 = float(input("Enter the starting point y0: "))
+
 xs, ys, fs = grad_descent(f, x0, y0, eta = 0.01, N = 1000)
 
 
 plt.figure()
 plt.plot(fs)
-plt.title("f(x,y)")
+plt.title("f(x,y) for over iterations")
 plt.xlabel("Iteration")
 plt.ylabel("f(x,y)")
 plt.grid(True)
 
+pad = 1.0
+x_min, x_max = xs.min() - pad, xs.max() + pad
+y_min, y_max = ys.min() - pad, ys.max() + pad
 
+xg = np.linspace(x_min, x_max, 200)
+yg = np.linspace(y_min, y_max, 200)
+X, Y = np.meshgrid(xg, yg)
+Z = f(X, Y)
 
 fig_3D = plt.figure()
 ax = fig_3D.add_subplot(111, projection='3d')
-ax.plot_surface(, Y, Z, alpha = 0.5)
-ax.plot(xs, ys, fs, linewidth=2)
+ax.plot_surface(X, Y, Z, alpha = 0.5)
+ax.plot(xs, ys, fs, linewidth = 2)
 ax.set_title("Surface + path")
 ax.set_xlabel("x")
 ax.set_ylabel("y")
