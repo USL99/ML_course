faf8c39c070d76401a7547d33635ad8d37eeb7cb
USL99 <USL99@mail.ru>
2026-03-14 10:32:11 +0300

    linear_regression p7 (add gradients)

--- 4_linear_regression_realization/descents.py
+++ 4_linear_regression_realization/descents.py
@@ -80,38 +80,71 @@ class StochasticGradientDescent(BaseDescent):
         gradient = self.model.compute_gradients(X_bs, y_bs)
         self.model.w = self.model.w - lr * gradient
 
-#
-#
-# class SAGDescent(BaseDescent):
-#     def __init__(self, lr_schedule: LearningRateSchedule = TimeDecayLR):
-#         super().__init__(lr_schedule)
-#         self.grad_memory = None
-#         self.grad_sum = None
-#
-#     def update_weights(self):
-#         # TODO: реализовать SAG
-#         X_train = self.model.X_train
-#         y_train = self.model.y_train
-#         num_objects, num_features = X_train.shape
-#
-#         if self.grad_memory is None:
-#             # TODO: инициализировать хранилища при первом вызове
-#
-#         # TODO: реализовать SAG
-#         raise NotImplementedError
-#
-#
-# class MomentumDescent(BaseDescent):
-#     def __init__(self, lr_schedule: LearningRateSchedule = TimeDecayLR, beta=0.9):
-#         super().__init__(lr_schedule)
-#         self.beta = beta
-#         self.velocity = None
-#
-#     def update_weights(self):
-#         # TODO: реализовать градиентный спуск с моментумом
-#         raise NotImplementedError
-#
-#
+
+class SAGDescent(BaseDescent):
+    def __init__(self, lr_schedule: LearningRateSchedule = TimeDecayLR, batch_size: int = 1):
+        super().__init__(lr_schedule)
+        self.batch_size = int (batch_size)
+        self.grad_memory = None
+        self.grad_sum = None
+
+    def update_weights(self):
+        X_train = self.model.X_train
+        y_train = self.model.y_train
+        n, d = X_train.shape
+
+        if self.grad_memory is None:
+            self.grad_memory = np.zeros((n, d), dtype = float)
+            self.grad_sum = np.zeros(d, dtype = float)
+
+        ind = np.random.randint(0, n, size = self.batch_size)
+
+        for j in np.atleast_ld(ind):
+            g_old = self.grad_memory[j].copy()
+            g_new = self.model.compute_gradients(X_train[j:j + 1], y_train[j:j + 1])  # градиент по 1 объекту
+            self.grad_memory[j] = g_new
+            self.grad_sum += (g_new - g_old)
+
+        avg_grad = self.grad_sum / n
+
+        lr = float(self.lr_schedule.get_lr(self.iteration))
+        self.model.w = self.model.w - lr * avg_grad
+
+        if getattr(self.model, "verbose", False) and (
+                self.iteration < 5 or self.iteration % getattr(self.model, "print_every", 10) == 0
+        ):
+            print(f"[SAG] iter={self.iteration:04d} lr={lr:.3g} "
+                  f"||avg_grad||={np.linalg.norm(avg_grad):.3g} ||w||={np.linalg.norm(self.model.w):.3g}")
+
+
+class MomentumDescent(BaseDescent):
+    def __init__(self, lr_schedule: LearningRateSchedule = TimeDecayLR, beta=0.9):
+        super().__init__(lr_schedule)
+        self.beta = float(beta)
+        self.velocity = None
+
+    def update_weights(self):
+        X_train = self.model.X_train
+        y_train = self.model.y_train
+
+        grad =  self.model.compute_gradients(X_train, y_train)
+
+        if self.velocity is None:
+            self.velocity = np.zeros_like(self.model.w)
+
+        lr = float(self.lr_schedule.get_lr(self.iteration))
+
+        self.velocity = self.beta * self.velocity + lr * grad
+
+        self.model.w = self.model.w - self.velocity
+
+        if getattr(self.model, "verbose", False) and (
+                self.iteration < 5 or self.iteration % getattr(self.model, "print_every", 10) == 0
+        ):
+            print(f"[Momentum] iter={self.iteration:04d} lr={lr:.3g} "
+                  f"||grad||={np.linalg.norm(grad):.3g} ||v||={np.linalg.norm(self.velocity):.3g} "
+                  f"||w||={np.linalg.norm(self.model.w):.3g}")
+
 # class Adam(BaseDescent):
 #     def __init__(self, lr_schedule: LearningRateSchedule = TimeDecayLR, beta1=0.9, beta2=0.999, eps=1e-8):
 #         super().__init__(lr_schedule)
